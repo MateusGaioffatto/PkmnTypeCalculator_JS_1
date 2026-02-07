a1ce12b8136776a46e4b1a2e96b5be2b1a0ad098
MateusGaioffatto <gaioffattomateus@gmail.com>
2026-02-07 13:04:28 -0300

    Added the Gigantamax passives for the Kanto, Unova and Galar Pokemons = 07/02/2026 | 13:05 AM

--- source/source_scripts/pokerogue_pokemonPassives/pokemon_Gigantamaxes_passives.js
+++ source/source_scripts/pokerogue_pokemonPassives/pokemon_Gigantamaxes_passives.js
@@ -2,8 +2,40 @@ export const pokerogue_KantoGigantamaxes_passives = {
   gigantamax_venusaur: "Flower Veil",
   gigantamax_charizard: "Unnerve",
   gigantamax_blastoise: "Bulletproof",
+  gigantamax_butterfree: "Magician",
+  gigantamax_pikachu: "Thick Fat",
   gigantamax_meowth: "Tough Claws",
   gigantamax_machamp: "Quick Feet",
   gigantamax_gengar: "Regenerator",
-  gigantamax_kingler: "Unburden"
+  gigantamax_kingler: "Unburden",
+  gigantamax_lapras: "Filter",
+  gigantamax_eevee: "Fluffy"
 };
+
+export const pokerogue_UnovaGigantamaxes_passives = {
+  gigantamax_garbodor: "Neutralizing Gas"
+};
+
+export const pokerogue_GalarGigantamaxes_passives = {
+  gigantamax_melmetal: "Full Metal Body",
+  gigantamax_rillaboom: "Grass Pelt",
+  gigantamax_cinderace: "Opportunist",
+  gigantamax_inteleon: "Super Luck",
+  gigantamax_corviknight: "Iron Barbs",
+  gigantamax_orbeetle: "Psychic Surge",
+  gigantamax_drednaw: "Solid Rock",
+  gigantamax_coalossal: "Solid Rock",
+  gigantamax_flapple: "No Guard",
+  gigantamax_appletun: "Well Baked Body",
+  gigantamax_sandaconda: "Sand Rush",
+  gigantamax_toxtricity: "Electric Surge",
+  gigantamax_centiskorch: "Hustle",
+  gigantamax_sinistea: "Shadow Shield",
+  gigantamax_hatterene: "Fairy Aura",
+  gigantamax_grimmsnarl: "Intimidate",
+  gigantamax_alcremie: "Regenerator",
+  gigantamax_copperajah: "Earth Eater",
+  gigantamax_duraludon: "Filter",
+  gigantamax_eternatus: "Neutralizing Gas",
+  gigantamax_urshifu: "Iron Fist"
+}
