070e3fef6a796468d0bb97241e1ae7ab77838853
MateusGaioffatto <gaioffattomateus@gmail.com>
2026-02-12 14:34:47 -0300

    Added egg moves for all Galar Pokémon evolutions + gigantamaxes + Galarian forms evolutions

--- source/source_scripts/pokerogue_pokemonEggMoves/pokemon_Galar_eggMoves.js
+++ source/source_scripts/pokerogue_pokemonEggMoves/pokemon_Galar_eggMoves.js
@@ -1,67 +1,199 @@
 export const pokerogue_pokemonEggMoves_Galar = {
   grookey: ["Rock Slide", "Play Rough", "Grassy Glide", "Clangorous Soul"],
+  thwackey: ["Rock Slide", "Play Rough", "Grassy Glide", "Clangorous Soul"],
+  rillaboom: ["Rock Slide", "Play Rough", "Grassy Glide", "Clangorous Soul"],
+  gigantamax_rillaboom: ["Rock Slide", "Play Rough", "Grassy Glide", "Clangorous Soul"],
+
   scorbunny: ["Extreme Speed", "High Jump Kick", "Supercell Slam", "Triple Axel"],
+  raboot: ["Extreme Speed", "High Jump Kick", "Supercell Slam", "Triple Axel"],
+  cinderace: ["Extreme Speed", "High Jump Kick", "Supercell Slam", "Triple Axel"],
+  gigantamax_cinderace: ["Extreme Speed", "High Jump Kick", "Supercell Slam", "Triple Axel"],
+
   sobble: ["Aeroblast", "Frost Breath", "Energy Ball", "Nasty Plot"],
+  drizzile: ["Aeroblast", "Frost Breath", "Energy Ball", "Nasty Plot"],
+  inteleon: ["Aeroblast", "Frost Breath", "Energy Ball", "Nasty Plot"],
+  gigantamax_inteleon: ["Aeroblast", "Frost Breath", "Energy Ball", "Nasty Plot"],
+
   skwovet: ["Sucker Punch", "Slack Off", "Coil", "Population Bomb"],
+  greedent: ["Sucker Punch", "Slack Off", "Coil", "Population Bomb"],
+
   rookidee: ["Roost", "Body Press", "Kings Shield", "Behemoth Bash"],
+  corvisquire: ["Roost", "Body Press", "Kings Shield", "Behemoth Bash"],
+  corviknight: ["Roost", "Body Press", "Kings Shield", "Behemoth Bash"],
+  gigantamax_corviknight: ["Roost", "Body Press", "Kings Shield", "Behemoth Bash"],
+
   blipbug: ["Heal Order", "Luster Purge", "Sleep Powder", "Tail Glow"],
+  dottler: ["Heal Order", "Luster Purge", "Sleep Powder", "Tail Glow"],
+  orbeetle: ["Heal Order", "Luster Purge", "Sleep Powder", "Tail Glow"],
+  gigantamax_orbeetle: ["Heal Order", "Luster Purge", "Sleep Powder", "Tail Glow"],
+
   nickit: ["Baddy Bad", "Mystical Fire", "Sparkly Swirl", "Make It Rain"],
+  thievul: ["Baddy Bad", "Mystical Fire", "Sparkly Swirl", "Make It Rain"],
+
   gossifleur: ["Baton Pass", "Tailwind", "Sappy Seed", "Spore"],
+  eldegoss: ["Baton Pass", "Tailwind", "Sappy Seed", "Spore"],
+
   wooloo: ["Nuzzle", "Milk Drink", "Body Press", "Multi Attack"],
+  dubwool: ["Nuzzle", "Milk Drink", "Body Press", "Multi Attack"],
+
   chewtle: ["Ice Fang", "Psychic Fangs", "Shell Smash", "Mighty Cleave"],
+  drednaw: ["Ice Fang", "Psychic Fangs", "Shell Smash", "Mighty Cleave"],
+  gigantamax_drednaw: ["Ice Fang", "Psychic Fangs", "Shell Smash", "Mighty Cleave"],
+
   yamper: ["Ice Fang", "Swords Dance", "Thunder Fang", "Bolt Strike"],
+  boltund: ["Ice Fang", "Swords Dance", "Thunder Fang", "Bolt Strike"],
+
   rolycoly: ["Blazing Torque", "Burning Bulwark", "Recover", "Diamond Storm"],
+  carkol: ["Blazing Torque", "Burning Bulwark", "Recover", "Diamond Storm"],
+  coalossal: ["Blazing Torque", "Burning Bulwark", "Recover", "Diamond Storm"],
+  gigantamax_coalossal: ["Blazing Torque", "Burning Bulwark", "Recover", "Diamond Storm"],
+
   applin: ["Core Enforcer", "Combat Torque", "Sappy Seed", "Matcha Gotcha"],
+  flapple: ["Core Enforcer", "Combat Torque", "Sappy Seed", "Matcha Gotcha"],
+  gigantamax_flapple: ["Core Enforcer", "Combat Torque", "Sappy Seed", "Matcha Gotcha"],
+  appletun: ["Core Enforcer", "Combat Torque", "Sappy Seed", "Matcha Gotcha"],
+  gigantamax_appletun: ["Core Enforcer", "Combat Torque", "Sappy Seed", "Matcha Gotcha"],
+
   silicobra: ["Shore Up", "Shed Tail", "Mountain Gale", "Thousand Arrows"],
+  sandaconda: ["Shore Up", "Shed Tail", "Mountain Gale", "Thousand Arrows"],
+  gigantamax_sandaconda: ["Shore Up", "Shed Tail", "Mountain Gale", "Thousand Arrows"],
+  
   cramorant: ["Apple Acid", "Surf", "Bolt Beak", "Oblivion Wing"],
+
   arrokuda: ["Supercell Slam", "Triple Dive", "Ice Spinner", "Swords Dance"],
+  barraskewda: ["Supercell Slam", "Triple Dive", "Ice Spinner", "Swords Dance"],
+
   toxel: ["Buzzy Buzz", "Bug Buzz", "Sparkling Aria", "Torch Song"],
+  amped_toxtricity: ["Buzzy Buzz", "Bug Buzz", "Sparkling Aria", "Torch Song"],
+  lowkey_toxtricity: ["Buzzy Buzz", "Bug Buzz", "Sparkling Aria", "Torch Song"],
+  gigantamax_toxtricity: ["Buzzy Buzz", "Bug Buzz", "Sparkling Aria", "Torch Song"],
+
   sizzlipede: ["Burning Bulwark", "Zing Zap", "First Impression", "Bitter Blade"],
+  centiskorch: ["Burning Bulwark", "Zing Zap", "First Impression", "Bitter Blade"],
+  gigantamax_centiskorch: ["Burning Bulwark", "Zing Zap", "First Impression", "Bitter Blade"],
+
   clobbopus: ["Storm Throw", "Jet Punch", "Mach Punch", "Surging Strikes"],
+  grapploct: ["Storm Throw", "Jet Punch", "Mach Punch", "Surging Strikes"],
+
   sinistea: ["Splishy Splash", "Matcha Gotcha", "Draining Kiss", "Moongeist Beam"],
+  polteageist: ["Splishy Splash", "Matcha Gotcha", "Draining Kiss", "Moongeist Beam"],
+
   hatenna: ["Recover", "Moonblast", "Buzzy Buzz", "Torch Song"],
+  hattrem: ["Recover", "Moonblast", "Buzzy Buzz", "Torch Song"],
+  hatterene: ["Recover", "Moonblast", "Buzzy Buzz", "Torch Song"],
+  gigantamax_hatterene: ["Recover", "Moonblast", "Buzzy Buzz", "Torch Song"],
+
   impidimp: ["Slack Off", "Parting Shot", "Octolock", "Wicked Blow"],
+  morgrem: ["Slack Off", "Parting Shot", "Octolock", "Wicked Blow"],
+  grimmsnarl: ["Slack Off", "Parting Shot", "Octolock", "Wicked Blow"],
+  gigantamax_grimmsnarl: ["Slack Off", "Parting Shot", "Octolock", "Wicked Blow"],
+  
   milcery: ["Moonblast", "Chilly Reception", "Earth Power", "Geomancy"],
+  alcremie: ["Moonblast", "Chilly Reception", "Earth Power", "Geomancy"],
+  gigantamax_alcremie: ["Moonblast", "Chilly Reception", "Earth Power", "Geomancy"],
+
   falinks: ["Baton Pass", "Power Trip", "Sacred Sword", "Heal Order"],
+  
   pincurchin: ["Trick Room", "Volt Switch", "Strength Sap", "Thunderclap"],
+
   snom: ["Frost Breath", "Heal Order", "Earth Power", "Spore"],
+  frosmoth: ["Frost Breath", "Heal Order", "Earth Power", "Spore"],
+  
   stonjourner: ["Axe Kick", "Helping Hand", "Accelerock", "Diamond Storm"],
+  
   eiscue: ["Triple Axel", "Aqua Step", "Axe Kick", "Shell Smash"],
+  
   indeedee: ["Matcha Gotcha", "Expanding Force", "Moonblast", "Revival Blessing"],
+  
   morpeko: ["Ice Spinner", "Obstruct", "Stuff Cheeks", "Collision Course"],
+
   cufant: ["Liquidation", "Curse", "Combat Torque", "Gigaton Hammer"],
+  copperajah: ["Liquidation", "Curse", "Combat Torque", "Gigaton Hammer"],
+  gigantamax_copperajah: ["Liquidation", "Curse", "Combat Torque", "Gigaton Hammer"],
+
   dracozolt: ["Triple Axel", "Gunk Shot", "Fire Lash", "Dragon Dance"],
   arctozolt: ["Mountain Gale", "Aqua Step", "High Horsepower", "Shift Gear"],
   dracovish: ["Triple Axel", "Dragon Hammer", "Thunder Fang", "Dragon Dance"],
   arctovish: ["Ice Fang", "Thunder Fang", "High Horsepower", "Shift Gear"],
+
   duraludon: ["Heat Wave", "Body Press", "Thunderclap", "Core Enforcer"],
+  gigantamax_duraludon: ["Heat Wave", "Body Press", "Thunderclap", "Core Enforcer"],
+
   dreepy: ["Shadow Bone", "Power Up Punch", "Fire Lash", "Dire Claw"],
+  drakloak: ["Shadow Bone", "Power Up Punch", "Fire Lash", "Dire Claw"],
+  dragapult: ["Shadow Bone", "Power Up Punch", "Fire Lash", "Dire Claw"],
+
   zacian: ["Magical Torque", "Mighty Cleave", "Earthquake", "Bitter Blade"],
+
   zamazenta: ["Bulk Up", "Body Press", "Power Trip", "Slack Off"],
+
   eternatus: ["Body Press", "Nasty Plot", "Malignant Chain", "Dragon Energy"],
+  gigantamax_eternatus: ["Body Press", "Nasty Plot", "Malignant Chain", "Dragon Energy"],
+
   kubfu: ["Meteor Mash", "Drain Punch", "Jet Punch", "Dragon Dance"],
+  single_strike_urshifu: ["Meteor Mash", "Drain Punch", "Jet Punch", "Dragon Dance"],
+  rapid_strike_urshifu: ["Meteor Mash", "Drain Punch", "Jet Punch", "Dragon Dance"],
+
   zarude: ["Leaf Blade", "Headlong Rush", "Wicked Blow", "Victory Dance"],
+
   regieleki: ["Nasty Plot", "Ice Beam", "Earth Power", "Electro Drift"],
+
   regidrago: ["Shell Side Arm", "Flamethrower", "Take Heart", "Dragon Darts"],
+
   glastrier: ["Speed Swap", "Slack Off", "High Horsepower", "Glacial Lance"],
+
   spectrier: ["Earth Power", "Moonlight", "Aura Sphere", "Astral Barrage"],
+
   calyrex: ["Sappy Seed", "Recover", "Secret Sword", "Photon Geyser"],
+
   enamorus: ["Aeroblast", "Thousand Arrows", "Stored Power", "Fleur Cannon"],
+
   galar_meowth: ["Liquidation", "Horn Leech", "Bullet Punch", "Behemoth Bash"],
+  perrserker: ["Liquidation", "Horn Leech", "Bullet Punch", "Behemoth Bash"],
+
   galar_ponyta: ["Magical Torque", "Extreme Speed", "Flare Blitz", "Photon Geyser"],
+  galar_rapidash: ["Magical Torque", "Extreme Speed", "Flare Blitz", "Photon Geyser"],
+
   galar_slowpoke: ["Shed Tail", "Baddy Bad", "Moonblast", "Photon Geyser"],
+  galar_slowbro: ["Shed Tail", "Baddy Bad", "Moonblast", "Photon Geyser"],
+  galar_slowking: ["Shed Tail", "Baddy Bad", "Moonblast", "Photon Geyser"],
+
   galar_farfetchd: ["Roost", "Sacred Sword", "Kings Shield", "Behemoth Blade"],
+  sirfetchd: ["Roost", "Sacred Sword", "Kings Shield", "Behemoth Blade"],
+
   galar_articuno: ["Secret Sword", "Night Daze", "Ice Beam", "Oblivion Wing"],
+
   galar_zapdos: ["Poison Jab", "Floaty Fall", "Roost", "Bolt Beak"],
+
   galar_moltres: ["Roost", "Sludge Bomb", "Flamethrower", "Oblivion Wing"],
+
   galar_corsola: ["Shell Smash", "Aura Sphere", "Infernal Parade", "Astral Barrage"],
+  cursola: ["Shell Smash", "Aura Sphere", "Infernal Parade", "Astral Barrage"],
+
   galar_zigzagoon: ["Ceaseless Edge", "Facade", "Parting Shot", "Extreme Speed"],
+  galar_linoone: ["Ceaseless Edge", "Facade", "Parting Shot", "Extreme Speed"],
+  obstagoon: ["Ceaseless Edge", "Facade", "Parting Shot", "Extreme Speed"],
+
   galar_darumaka: ["Ice Spinner", "Zing Zap", "Drain Punch", "Pyro Ball"],
+  galar_darmanitan: ["Ice Spinner", "Zing Zap", "Drain Punch", "Pyro Ball"],
+
   galar_yamask: ["Strength Sap", "Dire Claw", "Thousand Waves", "Spectral Thief"],
+  runerigus: ["Strength Sap", "Dire Claw", "Thousand Waves", "Spectral Thief"],
+
   galar_stunfisk: ["Spiky Shield", "Thousand Arrows", "Strength Sap", "Double Iron Bash"],
+
   hisui_growlithe: ["Wave Crash", "Head Smash", "Volt Tackle", "Dragon Dance"],
+  hisui_arcanine: ["Wave Crash", "Head Smash", "Volt Tackle", "Dragon Dance"],
+
   hisui_voltorb: ["Dazzling Gleam", "Leech Seed", "Apple Acid", "Mind Blown"],
+  hisui_electrode: ["Dazzling Gleam", "Leech Seed", "Apple Acid", "Mind Blown"],
+
   hisui_qwilfish: ["Ceaseless Edge", "Baneful Bunker", "Recover", "Fishious Rend"],
+  overqwil: ["Ceaseless Edge", "Baneful Bunker", "Recover", "Fishious Rend"],
+
   hisui_sneasel: ["Drain Punch", "Knock Off", "Psychic Fangs", "Triple Axel"],
-  hisui_zorua: ["Moonblast", "Secret Sword", "Parting Shot", "Blood Moon"]
+  sneasler: ["Drain Punch", "Knock Off", "Psychic Fangs", "Triple Axel"],
+
+  hisui_zorua: ["Moonblast", "Secret Sword", "Parting Shot", "Blood Moon"],
+  hisui_zoroark: ["Moonblast", "Secret Sword", "Parting Shot", "Blood Moon"]
 };
