ba7cfd7d423805136f6173796cb18d8c60254c12
MateusGaioffatto <gaioffattomateus@gmail.com>
2026-02-12 14:49:13 -0300

    Added egg moves for all Paldea Pokémon evolutions + Paldea forms evolutions

--- source/source_scripts/pokerogue_pokemonEggMoves/pokemon_Paldea_eggMoves.js
+++ source/source_scripts/pokerogue_pokemonEggMoves/pokemon_Paldea_eggMoves.js
@@ -1,77 +1,192 @@
 export const pokerogue_pokemonPaldea_eggMoves = {
   sprigatito: ["Fire Lash", "Triple Axel", "Sucker Punch", "Wicked Blow"],
+  floragato: ["Fire Lash", "Triple Axel", "Sucker Punch", "Wicked Blow"],
+  meowscarada: ["Fire Lash", "Triple Axel", "Sucker Punch", "Wicked Blow"],
+
   fuecoco: ["Alluring Voice", "Slack Off", "Overdrive", "Moongeist Beam"],
+  crocalor: ["Alluring Voice", "Slack Off", "Overdrive", "Moongeist Beam"],
+  skeledirge: ["Alluring Voice", "Slack Off", "Overdrive", "Moongeist Beam"],
+
   quaxly: ["Dragon Dance", "Triple Axel", "Power Trip", "Thunderous Kick"],
+  quaxwell: ["Dragon Dance", "Triple Axel", "Power Trip", "Thunderous Kick"],
+  quaquaval: ["Dragon Dance", "Triple Axel", "Power Trip", "Thunderous Kick"],
+
   lechonk: ["Milk Drink", "Psyshield Bash", "Blazing Torque", "Fillet Away"],
+  oinkologne: ["Milk Drink", "Psyshield Bash", "Blazing Torque", "Fillet Away"],
+
   tarountula: ["Stone Axe", "Leech Life", "Fake Out", "Spore"],
+  spidops: ["Stone Axe", "Leech Life", "Fake Out", "Spore"],
+
   nymble: ["Knock Off", "Fell Stinger", "Attack Order", "Wicked Blow"],
+  lokix: ["Knock Off", "Fell Stinger", "Attack Order", "Wicked Blow"],
+
   pawmi: ["Drain Punch", "Meteor Mash", "Jet Punch", "Plasma Fists"],
+  pawmo: ["Drain Punch", "Meteor Mash", "Jet Punch", "Plasma Fists"],
+  pawmot: ["Drain Punch", "Meteor Mash", "Jet Punch", "Plasma Fists"],
+
   tandemaus: ["Baton Pass", "Fake Out", "Power Up Punch", "Revival Blessing"],
+  maushold: ["Baton Pass", "Fake Out", "Power Up Punch", "Revival Blessing"],
+
   fidough: ["Soft Boiled", "High Horsepower", "Sizzly Slide", "Tidy Up"],
+  dachsbun: ["Soft Boiled", "High Horsepower", "Sizzly Slide", "Tidy Up"],
+
   smoliv: ["Strength Sap", "Earth Power", "Calm Mind", "Boomburst"],
+  dolliv: ["Strength Sap", "Earth Power", "Calm Mind", "Boomburst"],
+  arboliva: ["Strength Sap", "Earth Power", "Calm Mind", "Boomburst"],
+
   squawkabilly: ["Parting Shot", "Bulk Up", "Close Combat", "Extreme Speed"],
+
   nacli: ["Knock Off", "Toxic", "Sand Tomb", "Diamond Storm"],
+  naclstack: ["Knock Off", "Toxic", "Sand Tomb", "Diamond Storm"],
+  garganacl: ["Knock Off", "Toxic", "Sand Tomb", "Diamond Storm"],
+
   charcadet: ["Sacred Sword", "Photon Geyser", "Moonblast", "Spectral Thief"],
+  armarouge: ["Sacred Sword", "Photon Geyser", "Moonblast", "Spectral Thief"],
+  ceruledge: ["Sacred Sword", "Photon Geyser", "Moonblast", "Spectral Thief"],
+
   tadbulb: ["Parabolic Charge", "Scald", "Earth Power", "Electro Shot"],
+  bellibolt: ["Parabolic Charge", "Scald", "Earth Power", "Electro Shot"],
+
   wattrel: ["Nasty Plot", "Splishy Splash", "Sandsear Storm", "Wildbolt Storm"],
+  kilowattrel: ["Nasty Plot", "Splishy Splash", "Sandsear Storm", "Wildbolt Storm"],
+
   maschiff: ["Parting Shot", "Leech Life", "Psychic Fangs", "No Retreat"],
+  mabosstiff: ["Parting Shot", "Leech Life", "Psychic Fangs", "No Retreat"],
+
   shroodle: ["Gastro Acid", "Parting Shot", "Toxic", "Sketch"],
+  grafaiai: ["Gastro Acid", "Parting Shot", "Toxic", "Sketch"],
+
   bramblin: ["Tailwind", "Strength Sap", "Flower Trick", "Last Respects"],
+  brambleghast: ["Tailwind", "Strength Sap", "Flower Trick", "Last Respects"],
+
   toedscool: ["Strength Sap", "Topsy Turvy", "Sappy Seed", "Tail Glow"],
+  toedscruel: ["Strength Sap", "Topsy Turvy", "Sappy Seed", "Tail Glow"],
+
   klawf: ["Crabhammer", "Shore Up", "Mighty Cleave", "Shell Smash"],
+
   capsakid: ["Strength Sap", "Apple Acid", "Frost Breath", "Torch Song"],
+  scovillain: ["Strength Sap", "Apple Acid", "Frost Breath", "Torch Song"],
+
   rellor: ["Heal Block", "Recover", "Magic Powder", "Lumina Crash"],
+  rabsca: ["Heal Block", "Recover", "Magic Powder", "Lumina Crash"],
+
   flittle: ["Cosmic Power", "Aura Sphere", "Roost", "Fiery Dance"],
+  espathra: ["Cosmic Power", "Aura Sphere", "Roost", "Fiery Dance"],
+
   tinkatink: ["Magical Torque", "Pyro Ball", "Ivy Cudgel", "Shift Gear"],
+  tinkatuff: ["Magical Torque", "Pyro Ball", "Ivy Cudgel", "Shift Gear"],
+  tinkaton: ["Magical Torque", "Pyro Ball", "Ivy Cudgel", "Shift Gear"],
+
   wiglett: ["Shell Smash", "Icicle Crash", "Seed Bomb", "Surging Strikes"],
+  wugtrio: ["Shell Smash", "Icicle Crash", "Seed Bomb", "Surging Strikes"],
+
   bombirdier: ["Floaty Fall", "Swords Dance", "Sucker Punch", "Mighty Cleave"],
+
   finizen: ["Triple Axel", "Drain Punch", "Headlong Rush", "Surging Strikes"],
+  palafin: ["Triple Axel", "Drain Punch", "Headlong Rush", "Surging Strikes"],
+
   varoom: ["Combat Torque", "U Turn", "Blazing Torque", "Noxious Torque"],
+  revavroom: ["Combat Torque", "U Turn", "Blazing Torque", "Noxious Torque"],
+
   cyclizar: ["Parting Shot", "Fire Lash", "High Horsepower", "Magical Torque"],
+ 
   orthworm: ["Sizzly Slide", "Coil", "Body Press", "Shore Up"],
+ 
   glimmet: ["Calm Mind", "Giga Drain", "Fiery Dance", "Malignant Chain"],
+  glimmora: ["Calm Mind", "Giga Drain", "Fiery Dance", "Malignant Chain"],
+
   greavard: ["Shadow Bone", "Sizzly Slide", "Shore Up", "Collision Course"],
+  houndstone: ["Shadow Bone", "Sizzly Slide", "Shore Up", "Collision Course"],
+
   flamigo: ["Thunderous Kick", "Triple Axel", "Floaty Fall", "Victory Dance"],
+
   cetoddle: ["Zing Zap", "High Horsepower", "Slack Off", "Dragon Dance"],
+  cetitan: ["Zing Zap", "High Horsepower", "Slack Off", "Dragon Dance"],
+
   veluza: ["Psyblade", "Leaf Blade", "Ceaseless Edge", "Bitter Blade"],
+
   dondozo: ["Soft Boiled", "Sizzly Slide", "Breaking Swipe", "Salt Cure"],
+
   tatsugiri: ["Sludge Bomb", "Fillet Away", "Core Enforcer", "Steam Eruption"],
+
   great_tusk: ["Stone Axe", "Morning Sun", "Collision Course", "Shift Gear"],
+
   scream_tail: ["Torch Song", "Glitzy Glow", "Moonlight", "Sparkly Swirl"],
+
   brute_bonnet: ["Sappy Seed", "Strength Sap", "Earthquake", "Wicked Blow"],
+
   flutter_mane: ["Moonlight", "Nasty Plot", "Earth Power", "Moongeist Beam"],
+
   slither_wing: ["Rock Slide", "Thunderous Kick", "Sunsteel Strike", "Victory Dance"],
+
   sandy_shocks: ["Morning Sun", "Thunderclap", "Nasty Plot", "Ice Beam"],
+
   iron_treads: ["Fusion Bolt", "Shift Gear", "Shore Up", "Sunsteel Strike"],
+
   iron_bundle: ["Earth Power", "Splishy Splash", "Volt Switch", "Nasty Plot"],
+
   iron_hands: ["Drain Punch", "Bulk Up", "Plasma Fists", "Ice Hammer"],
+
   iron_jugulis: ["Fiery Wrath", "Roost", "Nasty Plot", "Oblivion Wing"],
+
   iron_moth: ["Earth Power", "Searing Shot", "Malignant Chain", "Quiver Dance"],
+
   iron_thorns: ["Diamond Storm", "Shore Up", "Shift Gear", "Plasma Fists"],
+
   frigibax: ["Behemoth Blade", "Dragon Dance", "Mountain Gale", "Precipice Blades"],
+  arctibax: ["Behemoth Blade", "Dragon Dance", "Mountain Gale", "Precipice Blades"],
+  baxcalibur: ["Behemoth Blade", "Dragon Dance", "Mountain Gale", "Precipice Blades"],
+
   gimmighoul: ["Happy Hour", "Aura Sphere", "Surf", "Astral Barrage"],
+  gholdengo: ["Happy Hour", "Aura Sphere", "Surf", "Astral Barrage"],
+
   wo_chien: ["Spore", "Fiery Wrath", "Sappy Seed", "Strength Sap"],
+
   chien_pao: ["Knock Off", "Parting Shot", "Triple Axel", "Bitter Blade"],
+
   ting_lu: ["Shore Up", "Ceaseless Edge", "Sappy Seed", "Precipice Blades"],
+
   chi_yu: ["Fiery Wrath", "Hydro Steam", "Morning Sun", "Blue Flare"],
+
   roaring_moon: ["Fire Lash", "Dragon Hammer", "Meteor Mash", "Dragon Ascent"],
+
   iron_valiant: ["Photon Geyser", "No Retreat", "Secret Sword", "Magical Torque"],
+
   koraidon: ["Sunsteel Strike", "Solar Blade", "Dragon Darts", "Bitter Blade"],
+
   miraidon: ["Frost Breath", "Wildbolt Storm", "Spacial Rend", "Rising Voltage"],
+
   walking_wake: ["Bouncy Bubble", "Fusion Flare", "Sludge Wave", "Core Enforcer"],
+
   iron_leaves: ["Bitter Blade", "U Turn", "Mighty Cleave", "Victory Dance"],
+
   poltchageist: ["Parabolic Charge", "Bouncy Bubble", "Leech Seed", "Sparkly Swirl"],
+  sinistcha: ["Parabolic Charge", "Bouncy Bubble", "Leech Seed", "Sparkly Swirl"],
+
   okidogi: ["Combat Torque", "Tidy Up", "Dire Claw", "Wicked Blow"],
+
   munkidori: ["Twin Beam", "Heat Wave", "Earth Power", "Malignant Chain"],
+
   fezandipiti: ["Barb Barrage", "Bonemerang", "Triple Axel", "Victory Dance"],
+
   ogerpon: ["Sleep Powder", "Bonemerang", "Triple Axel", "Flower Trick"],
+
   gouging_fire: ["Extreme Speed", "Dragon Dance", "Zing Zap", "Sacred Fire"],
+
   raging_bolt: ["Nasty Plot", "Flamethrower", "Morning Sun", "Electro Drift"],
+
   iron_boulder: ["Psyblade", "Kowtow Cleave", "Stone Axe", "Bitter Blade"],
+
   iron_crown: ["Nasty Plot", "Secret Sword", "Psystrike", "Electro Drift"],
+
   terapagos: ["Moonblast", "Nasty Plot", "Astral Barrage", "Recover"],
+
   pecharunt: ["Take Heart", "Body Press", "Sappy Seed", "Astral Barrage"],
+
   paldea_tauros: ["U Turn", "Sucker Punch", "Horn Leech", "Thunderous Kick"],
+
   paldea_wooper: ["Stone Axe", "Recover", "Baneful Bunker", "Barb Barrage"],
+  clodsire: ["Stone Axe", "Recover", "Baneful Bunker", "Barb Barrage"],
+
   bloodmoon_ursaluna: ["Nasty Plot", "Rock Polish", "Sandsear Storm", "Boomburst"]
 };
