2555d2fb29be863aad9a07cf701cb1cf19fadeaf
MateusGaioffatto <gaioffattomateus@gmail.com>
2026-02-12 13:07:07 -0300

    Added egg moves for all Alola Pokémon evolutions + Alola forms evolutions

--- source/source_scripts/pokerogue_pokemonEggMoves/pokemon_Alola_eggMoves.js
+++ source/source_scripts/pokerogue_pokemonEggMoves/pokemon_Alola_eggMoves.js
@@ -1,64 +1,164 @@
 export const pokerogue_pokemonEggMoves_Alola = {
   rowlet: ["Thousand Arrows", "Shadow Bone", "First Impression", "Victory Dance"],
+  dartrix: ["Thousand Arrows", "Shadow Bone", "First Impression", "Victory Dance"],
+  decidueye: ["Thousand Arrows", "Shadow Bone", "First Impression", "Victory Dance"],
+
   litten: ["Sucker Punch", "Parting Shot", "Slack Off", "Sacred Fire"],
+  torracat: ["Sucker Punch", "Parting Shot", "Slack Off", "Sacred Fire"],
+  incineroar: ["Sucker Punch", "Parting Shot", "Slack Off", "Sacred Fire"],
+
   popplio: ["Psychic Noise", "Moonlight", "Overdrive", "Torch Song"],
+  brionne: ["Psychic Noise", "Moonlight", "Overdrive", "Torch Song"],
+  primarina: ["Psychic Noise", "Moonlight", "Overdrive", "Torch Song"],
+
   pikipek: ["Trailblaze", "Bone Rush", "Burning Bulwark", "Population Bomb"],
+  trumbeak: ["Trailblaze", "Bone Rush", "Burning Bulwark", "Population Bomb"],
+  toucannon: ["Trailblaze", "Bone Rush", "Burning Bulwark", "Population Bomb"],
+
   yungoos: ["Fake Out", "High Horsepower", "Tidy Up", "Extreme Speed"],
+  gumshoos: ["Fake Out", "High Horsepower", "Tidy Up", "Extreme Speed"],
+
   grubbin: ["Thunderclap", "Earth Power", "Calm Mind", "Ice Beam"],
+  charjabug: ["Thunderclap", "Earth Power", "Calm Mind", "Ice Beam"],
+  vikavolt: ["Thunderclap", "Earth Power", "Calm Mind", "Ice Beam"],
+
   crabrawler: ["Jet Punch", "Shore Up", "Mach Punch", "Surging Strikes"],
+  crabominable: ["Jet Punch", "Shore Up", "Mach Punch", "Surging Strikes"],
+
   oricorio: ["Quiver Dance", "Fiery Dance", "Thunderclap", "Oblivion Wing"],
+
   cutiefly: ["Sticky Web", "Sleep Powder", "Heat Wave", "Sparkly Swirl"],
+  ribombee: ["Sticky Web", "Sleep Powder", "Heat Wave", "Sparkly Swirl"],
+
   rockruff: ["High Horsepower", "Tidy Up", "Ice Spinner", "Mighty Cleave"],
+  lycanroc_midday: ["High Horsepower", "Tidy Up", "Ice Spinner", "Mighty Cleave"],
+  lycanroc_midnight: ["High Horsepower", "Tidy Up", "Ice Spinner", "Mighty Cleave"],
+  lycanroc_dusk: ["High Horsepower", "Tidy Up", "Ice Spinner", "Mighty Cleave"],
+  
   wishiwashi: ["Heal Order", "Freeze Dry", "Water Shuriken", "Tail Glow"],
+
   mareanie: ["Ceaseless Edge", "Barb Barrage", "Body Press", "Flip Turn"],
+  toxapex: ["Ceaseless Edge", "Barb Barrage", "Body Press", "Flip Turn"],
+
   mudbray: ["Body Press", "Yawn", "Shore Up", "Thousand Waves"],
+  mudsdale: ["Body Press", "Yawn", "Shore Up", "Thousand Waves"],
+
   dewpider: ["Aqua Step", "Silk Trap", "Swords Dance", "Jet Punch"],
+  araquanid: ["Aqua Step", "Silk Trap", "Swords Dance", "Jet Punch"],
+
   fomantis: ["Superpower", "Headlong Rush", "Ice Hammer", "Bitter Blade"],
+  lurantis: ["Superpower", "Headlong Rush", "Ice Hammer", "Bitter Blade"],
+
   morelull: ["Calm Mind", "Sappy Seed", "Draining Kiss", "Matcha Gotcha"],
+  shiinotic: ["Calm Mind", "Sappy Seed", "Draining Kiss", "Matcha Gotcha"],
+
   salandit: ["Scald", "Malignant Chain", "Core Enforcer", "Eruption"],
+  salazzle: ["Scald", "Malignant Chain", "Core Enforcer", "Eruption"],
+
   stufful: ["Drain Punch", "Meteor Mash", "Ice Hammer", "Crush Grip"],
+  bewear: ["Drain Punch", "Meteor Mash", "Ice Hammer", "Crush Grip"],
+
   bounsweet: ["Triple Axel", "Aqua Step", "Thunderous Kick", "Flower Trick"],
+  steenee: ["Triple Axel", "Aqua Step", "Thunderous Kick", "Flower Trick"],
+  tsareena: ["Triple Axel", "Aqua Step", "Thunderous Kick", "Flower Trick"],
+
   comfey: ["Revival Blessing", "Take Heart", "Strength Sap", "Matcha Gotcha"],
+
   oranguru: ["Jungle Healing", "Yawn", "Follow Me", "Lumina Crash"],
+
   passimian: ["Pyro Ball", "Sucker Punch", "Zing Zap", "Victory Dance"],
+
   wimpod: ["Triple Axel", "Obstruct", "Jet Punch", "Surging Strikes"],
+  golisopod: ["Triple Axel", "Obstruct", "Jet Punch", "Surging Strikes"],
+
   sandygast: ["Bitter Malice", "Splishy Splash", "Take Heart", "Salt Cure"],
+  palossand: ["Bitter Malice", "Splishy Splash", "Take Heart", "Salt Cure"],
+
   pyukumuku: ["Comeuppance", "Baneful Bunker", "Toxic Spikes", "Salt Cure"],
+
   type_null: ["Dire Claw", "Recover", "Combat Torque", "No Retreat"],
+
   minior: ["Earth Power", "Floaty Fall", "Tri Attack", "Diamond Storm"],
+
   komala: ["Slack Off", "Extreme Speed", "Knock Off", "Collision Course"],
+
   turtonator: ["Burning Bulwark", "Morning Sun", "Body Press", "Core Enforcer"],
+
   togedemaru: ["Fake Out", "Metal Burst", "Meteor Mash", "Aura Wheel"],
+
   mimikyu: ["Spirit Break", "Tidy Up", "Fire Lash", "Spectral Thief"],
+
   bruxish: ["Play Rough", "Fire Fang", "Dragon Dance", "Surging Strikes"],
+
   drampa: ["Slack Off", "Trick Room", "Core Enforcer", "Boomburst"],
+
   dhelmise: ["Shadow Bone", "Ivy Cudgel", "Triple Dive", "Strength Sap"],
+  
   jangmo_o: ["Body Press", "Shell Side Arm", "Secret Sword", "Glaive Rush"],
+  hakamo_o: ["Body Press", "Shell Side Arm", "Secret Sword", "Glaive Rush"],
+  kommo_o: ["Body Press", "Shell Side Arm", "Secret Sword", "Glaive Rush"],
+
   tapu_koko: ["Play Rough", "Zing Zap", "Swords Dance", "Triple Axel"],
+
   tapu_lele: ["Moonlight", "Nasty Plot", "Heat Wave", "Expanding Force"],
+
   tapu_bulu: ["Sappy Seed", "Lands Wrath", "Magical Torque", "Victory Dance"],
+
   tapu_fini: ["Springtide Storm", "Earth Power", "Recover", "Quiver Dance"],
+
   cosmog: ["Photon Geyser", "Precipice Blades", "Sacred Fire", "Astral Barrage"],
+  cosmoem: ["Photon Geyser", "Precipice Blades", "Sacred Fire", "Astral Barrage"],
+  solgaleo: ["Sunsteel Strike", "Morning Sun", "Flare Blitz", "Zen Headbutt"],
+  lunala: ["Moongeist Beam", "Moonlight", "Shadow Ball", "Psychic"],
+
   nihilego: ["Strength Sap", "Malignant Chain", "Earth Power", "Quiver Dance"],
+
   buzzwole: ["First Impression", "Combat Torque", "Rock Blast", "Double Iron Bash"],
+
   pheromosa: ["Secret Sword", "Make It Rain", "Attack Order", "Diamond Storm"],
+
   xurkitree: ["Flamethrower", "Giga Drain", "Tail Glow", "Thunderclap"],
+
   celesteela: ["Recover", "Buzzy Buzz", "Earth Power", "Oblivion Wing"],
+
   kartana: ["Mighty Cleave", "Dual Chop", "Behemoth Blade", "Bitter Blade"],
+
   guzzlord: ["Sucker Punch", "Comeuppance", "Slack Off", "Shed Tail"],
+
   necrozma: ["Dynamax Cannon", "Sacred Fire", "Astral Barrage", "Clangorous Soul"],
+
   magearna: ["Recover", "Earth Power", "Moonblast", "Make It Rain"],
+
   marshadow: ["Power Up Punch", "Bonemerang", "Meteor Mash", "Triple Axel"],
+
   poipole: ["Malignant Chain", "Ice Beam", "Armor Cannon", "Clanging Scales"],
+  naganadel: ["Malignant Chain", "Ice Beam", "Armor Cannon", "Clanging Scales"],
+
   stakataka: ["Heavy Slam", "Heal Order", "Curse", "Salt Cure"],
+
   blacephalon: ["Chilly Reception", "Moonblast", "Energy Ball", "Moongeist Beam"],
+
   zeraora: ["Swords Dance", "Fire Lash", "Collision Course", "Triple Axel"],
+
   meltan: ["Bullet Punch", "Drain Punch", "Bulk Up", "Plasma Fists"],
-  alola_rattata: ["False Surrender", "Psychic Fangs", "Coil", "Extreme Speed"],
+  melmetal: ["Bullet Punch", "Drain Punch", "Bulk Up", "Plasma Fists"],
+
   alola_sandshrew: ["Spiky Shield", "Liquidation", "Shift Gear", "Glacial Lance"],
+  alola_sandslash: ["Spiky Shield", "Liquidation", "Shift Gear", "Glacial Lance"],
+
   alola_vulpix: ["Moonblast", "Glare", "Mystical Fire", "Lunar Blessing"],
+  alola_ninetales: ["Moonblast", "Glare", "Mystical Fire", "Lunar Blessing"],
+
   alola_diglett: ["Thousand Waves", "Swords Dance", "Triple Dive", "Pyro Ball"],
+  alola_dugtrio: ["Thousand Waves", "Swords Dance", "Triple Dive", "Pyro Ball"],
+
   alola_meowth: ["Baddy Bad", "Buzzy Buzz", "Parting Shot", "Make It Rain"],
+  alola_persian: ["Baddy Bad", "Buzzy Buzz", "Parting Shot", "Make It Rain"],
+
   alola_geodude: ["Lands Wrath", "Fusion Bolt", "Stone Axe", "Extreme Speed"],
-  alola_grimer: ["Sucker Punch", "Barb Barrage", "Recover", "Surging Strikes"]
+  alola_graveler: ["Lands Wrath", "Fusion Bolt", "Stone Axe", "Extreme Speed"],
+  alola_golem: ["Lands Wrath", "Fusion Bolt", "Stone Axe", "Extreme Speed"],
+
+  alola_grimer: ["Sucker Punch", "Barb Barrage", "Recover", "Surging Strikes"],
+  alola_muk: ["Sucker Punch", "Barb Barrage", "Recover", "Surging Strikes"]
 }
