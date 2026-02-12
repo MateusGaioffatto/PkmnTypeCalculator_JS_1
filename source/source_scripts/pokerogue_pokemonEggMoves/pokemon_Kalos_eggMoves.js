67705d128a4d972d232ddaa4230ee704da96ecec
MateusGaioffatto <gaioffattomateus@gmail.com>
2026-02-12 12:51:21 -0300

    Added egg moves for all Kalos Pokémon evolutions + mega forms

--- source/source_scripts/pokerogue_pokemonEggMoves/pokemon_Kalos_eggMoves.js
+++ source/source_scripts/pokerogue_pokemonEggMoves/pokemon_Kalos_eggMoves.js
@@ -1,40 +1,111 @@
-export const pokerogue_pokemonEggMoves_Kalos = {
+export const pokerogue_pokemonEggMoves_Kalos = {  
   chespin: ["Combat Torque", "Synthesis", "Ceaseless Edge", "Sappy Seed"],
+  quilladin: ["Combat Torque", "Synthesis", "Ceaseless Edge", "Sappy Seed"],
+  chesnaught: ["Combat Torque", "Synthesis", "Ceaseless Edge", "Sappy Seed"],
+  
   fennekin: ["Twin Beam", "Fiery Dance", "Thunderbolt", "Sparkly Swirl"],
+  braixen: ["Twin Beam", "Fiery Dance", "Thunderbolt", "Sparkly Swirl"],
+  delphox: ["Twin Beam", "Fiery Dance", "Thunderbolt", "Sparkly Swirl"],
+  
   froakie: ["Moonblast", "Shell Side Arm", "Fiery Wrath", "Steam Eruption"],
+  frogadier: ["Moonblast", "Shell Side Arm", "Fiery Wrath", "Steam Eruption"],
+  greninja: ["Moonblast", "Shell Side Arm", "Fiery Wrath", "Steam Eruption"],
+  
   bunnelby: ["Drain Punch", "Tidy Up", "Lands Wrath", "Extreme Speed"],
+  diggersby: ["Drain Punch", "Tidy Up", "Lands Wrath", "Extreme Speed"],
+
   fletchling: ["Drill Run", "Burning Bulwark", "Head Smash", "Volt Tackle"],
+  fletchinder: ["Drill Run", "Burning Bulwark", "Head Smash", "Volt Tackle"],
+  talonflame: ["Drill Run", "Burning Bulwark", "Head Smash", "Volt Tackle"],
+
   scatterbug: ["Focus Blast", "After You", "Decorate", "Blizzard"],
+  spewpa: ["Focus Blast", "After You", "Decorate", "Blizzard"],
+  vivillon: ["Focus Blast", "After You", "Decorate", "Blizzard"],
+  
   litleo: ["Earth Power", "Take Heart", "Burning Bulwark", "Boomburst"],
+  pyroar: ["Earth Power", "Take Heart", "Burning Bulwark", "Boomburst"],
+
   flabebe: ["Glitzy Glow", "Mystical Fire", "Take Heart", "Seed Flare"],
+  floette: ["Glitzy Glow", "Mystical Fire", "Take Heart", "Seed Flare"],
+  florges: ["Glitzy Glow", "Mystical Fire", "Take Heart", "Seed Flare"],
+  eternal_floette: ["Mystical Fire", "Chloroblast", "Luster Purge", "Quiver Dance"],
+  
   skiddo: ["High Horsepower", "Grassy Glide", "Stone Axe", "Sappy Seed"],
+  gogoat: ["High Horsepower", "Grassy Glide", "Stone Axe", "Sappy Seed"],
+  
   pancham: ["Drain Punch", "Sucker Punch", "Meteor Mash", "Wicked Blow"],
+  pangoro: ["Drain Punch", "Sucker Punch", "Meteor Mash", "Wicked Blow"],
+  
   furfrou: ["Tidy Up", "Slack Off", "Combat Torque", "Multi Attack"],
+  
   espurr: ["Luster Purge", "Moonblast", "Aura Sphere", "Dark Void"],
+  meowstic: ["Luster Purge", "Moonblast", "Aura Sphere", "Dark Void"],
+  
   honedge: ["Tachyon Cutter", "Spirit Shackle", "Bitter Blade", "Behemoth Blade"],
+  doublade: ["Tachyon Cutter", "Spirit Shackle", "Bitter Blade", "Behemoth Blade"],
+  aegislash: ["Tachyon Cutter", "Spirit Shackle", "Bitter Blade", "Behemoth Blade"],
+  
   spritzee: ["Speed Swap", "Revival Blessing", "Roost", "Torch Song"],
+  aromatisse: ["Speed Swap", "Revival Blessing", "Roost", "Torch Song"],
+  
   swirlix: ["Belly Drum", "Headlong Rush", "Magical Torque", "Revival Blessing"],
+  slurpuff: ["Belly Drum", "Headlong Rush", "Magical Torque", "Revival Blessing"],
+  
   inkay: ["Power Trip", "Spin Out", "Recover", "Psycho Boost"],
+  malamar: ["Power Trip", "Spin Out", "Recover", "Psycho Boost"],
+  
   binacle: ["Ice Punch", "Crabhammer", "Meteor Mash", "Mighty Cleave"],
+  barbaracle: ["Ice Punch", "Crabhammer", "Meteor Mash", "Mighty Cleave"],
+  
   skrelp: ["Strength Sap", "Trick Room", "Calm Mind", "Core Enforcer"],
+  dragalge: ["Strength Sap", "Trick Room", "Calm Mind", "Core Enforcer"],
+  
   clauncher: ["Shell Smash", "Armor Cannon", "Energy Ball", "Origin Pulse"],
+  clawitzer: ["Shell Smash", "Armor Cannon", "Energy Ball", "Origin Pulse"],
+  
   helioptile: ["Weather Ball", "Hydro Steam", "Earth Power", "Boomburst"],
+  heliolisk: ["Weather Ball", "Hydro Steam", "Earth Power", "Boomburst"],
+  
   tyrunt: ["Dragon Hammer", "Flare Blitz", "Volt Tackle", "Shift Gear"],
+  tyrantrum: ["Dragon Hammer", "Flare Blitz", "Volt Tackle", "Shift Gear"],
+  
   amaura: ["Recover", "Tera Starstorm", "Power Gem", "Geomancy"],
+  aurorus: ["Recover", "Tera Starstorm", "Power Gem", "Geomancy"],
+  
   hawlucha: ["Triple Axel", "High Horsepower", "Floaty Fall", "Wicked Blow"],
+  
   dedenne: ["Boomburst", "Fake Out", "Nasty Plot", "Revival Blessing"],
+  
   carbink: ["Body Press", "Shore Up", "Sparkly Swirl", "Diamond Storm"],
+  
   goomy: ["Dragon Hammer", "Recover", "Calm Mind", "Make It Rain"],
+  sliggoo: ["Dragon Hammer", "Recover", "Calm Mind", "Make It Rain"],
+  goodra: ["Dragon Hammer", "Recover", "Calm Mind", "Make It Rain"],
+  
   klefki: ["Heal Bell", "Encore", "Instruct", "Topsy Turvy"],
+  
   phantump: ["Rage Fist", "Sleep Powder", "Bulk Up", "Sappy Seed"],
+  trevenant: ["Rage Fist", "Sleep Powder", "Bulk Up", "Sappy Seed"],
+  
   pumpkaboo: ["Spirit Shackle", "Fire Lash", "Dire Claw", "Sappy Seed"],
+  gourgeist: ["Spirit Shackle", "Fire Lash", "Dire Claw", "Sappy Seed"],
+  
   bergmite: ["Stone Axe", "Metal Burst", "Body Press", "Glacial Lance"],
+  avalugg: ["Stone Axe", "Metal Burst", "Body Press", "Glacial Lance"],
+  
   noibat: ["Aeroblast", "Overdrive", "Nasty Plot", "Clanging Scales"],
+  noivern: ["Aeroblast", "Overdrive", "Nasty Plot", "Clanging Scales"],
+  
   xerneas: ["Earth Power", "Springtide Storm", "Stored Power", "Strength Sap"],
+
   yveltal: ["Sludge Wave", "Power Trip", "Fiery Wrath", "Clangorous Soul"],
+
   zygarde: ["Dragon Darts", "V Create", "Clangorous Soul", "Heal Order"],
+
   diancie: ["Magical Torque", "Fiery Dance", "Shore Up", "Geomancy"],
+  mega_diancie: ["Magical Torque", "Fiery Dance", "Shore Up", "Geomancy"],
+
   hoopa: ["Photon Geyser", "Secret Sword", "Fiery Wrath", "Shell Smash"],
-  volcanion: ["Hydro Steam", "Calm Mind", "Searing Shot", "Thunderclap"],
-  eternal_floette: ["Mystical Fire", "Chloroblast", "Luster Purge", "Quiver Dance"]
+
+  volcanion: ["Hydro Steam", "Calm Mind", "Searing Shot", "Thunderclap"]
 };
