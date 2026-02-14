acadba627d9c8d3e34a1347e2da7a3b45debcd29
MateusGaioffatto <gaioffattomateus@gmail.com>
2026-02-14 14:38:32 -0300

    Refactor: Updated mega & primal forms names on the #pokemon_Hoenn_eggMoves file

--- source/source_scripts/pokerogue_pokemonEggMoves/pokemon_Hoenn_eggMoves.js
+++ source/source_scripts/pokerogue_pokemonEggMoves/pokemon_Hoenn_eggMoves.js
@@ -2,17 +2,17 @@ export const pokerogue_pokemonEggMoves_Hoenn = {
   treecko: ["Nasty Plot", "Clanging Scales", "Secret Sword", "Seed Flare"],
   grovyle: ["Nasty Plot", "Clanging Scales", "Secret Sword", "Seed Flare"],
   sceptile: ["Nasty Plot", "Clanging Scales", "Secret Sword", "Seed Flare"],
-  mega_sceptile: ["Nasty Plot", "Clanging Scales", "Secret Sword", "Seed Flare"],
+  sceptile_mega: ["Nasty Plot", "Clanging Scales", "Secret Sword", "Seed Flare"],
 
   torchic: ["Thunderous Kick", "Zing Zap", "Burning Bulwark", "Pyro Ball"],
   combusken: ["Thunderous Kick", "Zing Zap", "Burning Bulwark", "Pyro Ball"],
   blaziken: ["Thunderous Kick", "Zing Zap", "Burning Bulwark", "Pyro Ball"],
-  mega_blaziken: ["Thunderous Kick", "Zing Zap", "Burning Bulwark", "Pyro Ball"],
+  blaziken_mega: ["Thunderous Kick", "Zing Zap", "Burning Bulwark", "Pyro Ball"],
 
   mudkip: ["Shore Up", "Mountain Gale", "Aqua Step", "Precipice Blades"],
   marshtomp: ["Shore Up", "Mountain Gale", "Aqua Step", "Precipice Blades"],
   swampert: ["Shore Up", "Mountain Gale", "Aqua Step", "Precipice Blades"],
-  mega_swampert: ["Shore Up", "Mountain Gale", "Aqua Step", "Precipice Blades"],
+  swampert_mega: ["Shore Up", "Mountain Gale", "Aqua Step", "Precipice Blades"],
 
   poochyena: ["Knock Off", "Close Combat", "Dire Claw", "Victory Dance"],
   mightyena: ["Knock Off", "Close Combat", "Dire Claw", "Victory Dance"],
@@ -44,9 +44,9 @@ export const pokerogue_pokemonEggMoves_Hoenn = {
   ralts: ["Psyblade", "Bitter Blade", "No Retreat", "Boomburst"],
   kirlia: ["Psyblade", "Bitter Blade", "No Retreat", "Boomburst"],
   gardevoir: ["Psyblade", "Bitter Blade", "No Retreat", "Boomburst"],
-  mega_gardevoir: ["Psyblade", "Bitter Blade", "No Retreat", "Boomburst"],
+  gardevoir_mega: ["Psyblade", "Bitter Blade", "No Retreat", "Boomburst"],
   gallade: ["Psyblade", "Bitter Blade", "No Retreat", "Boomburst"],
-  mega_gallade: ["Psyblade", "Bitter Blade", "No Retreat", "Boomburst"],
+  gallade_mega: ["Psyblade", "Bitter Blade", "No Retreat", "Boomburst"],
 
   surskit: ["Pollen Puff", "Fiery Dance", "Bouncy Bubble", "Aeroblast"],
   masquerain: ["Pollen Puff", "Fiery Dance", "Bouncy Bubble", "Aeroblast"],
@@ -80,23 +80,23 @@ export const pokerogue_pokemonEggMoves_Hoenn = {
   delcatty: ["Thunderous Kick", "Entrainment", "Tidy Up", "V Create"],
 
   sableye: ["Recover", "Topsy Turvy", "Curse", "Salt Cure"],
-  mega_sableye: ["Recover", "Topsy Turvy", "Curse", "Salt Cure"],
+  sableye_mega: ["Recover", "Topsy Turvy", "Curse", "Salt Cure"],
 
   mawile: ["Bullet Punch", "Horn Leech", "Earthquake", "Magical Torque"],
-  mega_mawile: ["Bullet Punch", "Horn Leech", "Earthquake", "Magical Torque"],
+  mawile_mega: ["Bullet Punch", "Horn Leech", "Earthquake", "Magical Torque"],
 
   aron: ["Head Smash", "Body Press", "Shore Up", "Salt Cure"],
   lairon: ["Head Smash", "Body Press", "Shore Up", "Salt Cure"],
   aggron: ["Head Smash", "Body Press", "Shore Up", "Salt Cure"],
-  mega_aggron: ["Head Smash", "Body Press", "Shore Up", "Salt Cure"],
+  aggron_mega: ["Head Smash", "Body Press", "Shore Up", "Salt Cure"],
 
   meditite: ["Thunderous Kick", "Sucker Punch", "Bullet Punch", "Photon Geyser"],
   medicham: ["Thunderous Kick", "Sucker Punch", "Bullet Punch", "Photon Geyser"],
-  mega_medicham: ["Thunderous Kick", "Sucker Punch", "Bullet Punch", "Photon Geyser"],
+  medicham_mega: ["Thunderous Kick", "Sucker Punch", "Bullet Punch", "Photon Geyser"],
 
   electrike: ["Frost Breath", "Heat Wave", "Nasty Plot", "Electro Drift"],
   manectric: ["Frost Breath", "Heat Wave", "Nasty Plot", "Electro Drift"],
-  mega_manectric: ["Frost Breath", "Heat Wave", "Nasty Plot", "Electro Drift"],
+  manectric_mega: ["Frost Breath", "Heat Wave", "Nasty Plot", "Electro Drift"],
 
   plusle: ["Flamethrower", "Glitzy Glow", "Splishy Splash", "Tail Glow"],
 
@@ -111,14 +111,14 @@ export const pokerogue_pokemonEggMoves_Hoenn = {
 
   carvanha: ["Thunder Fang", "Gunk Shot", "Obstruct", "Surging Strikes"],
   sharpedo: ["Thunder Fang", "Gunk Shot", "Obstruct", "Surging Strikes"],
-  mega_sharpedo: ["Thunder Fang", "Gunk Shot", "Obstruct", "Surging Strikes"],
+  sharpedo_mega: ["Thunder Fang", "Gunk Shot", "Obstruct", "Surging Strikes"],
 
   wailmer: ["Take Heart", "Comeuppance", "Slack Off", "Steam Eruption"],
   wailord: ["Take Heart", "Comeuppance", "Slack Off", "Steam Eruption"],
 
   numel: ["Trick Room", "Energy Ball", "Slack Off", "Blue Flare"],
   camerupt: ["Trick Room", "Energy Ball", "Slack Off", "Blue Flare"],
-  mega_camerupt: ["Trick Room", "Energy Ball", "Slack Off", "Blue Flare"],
+  camerupt_mega: ["Trick Room", "Energy Ball", "Slack Off", "Blue Flare"],
 
   torkoal: ["Morning Sun", "Burning Bulwark", "Body Press", "Hydro Steam"],
 
@@ -136,7 +136,7 @@ export const pokerogue_pokemonEggMoves_Hoenn = {
 
   swablu: ["Roost", "Take Heart", "Aeroblast", "Boomburst"],
   altaria: ["Roost", "Take Heart", "Aeroblast", "Boomburst"],
-  mega_altaria: ["Roost", "Take Heart", "Aeroblast", "Boomburst"],
+  altaria_mega: ["Roost", "Take Heart", "Aeroblast", "Boomburst"],
 
   zangoose: ["Facade", "High Horsepower", "Extreme Speed", "Tidy Up"],
 
@@ -170,7 +170,7 @@ export const pokerogue_pokemonEggMoves_Hoenn = {
 
   shuppet: ["Storm Throw", "Tidy Up", "Parting Shot", "Spectral Thief"],
   banette: ["Storm Throw", "Tidy Up", "Parting Shot", "Spectral Thief"],
-  mega_banette: ["Storm Throw", "Tidy Up", "Parting Shot", "Spectral Thief"],
+  banette_mega: ["Storm Throw", "Tidy Up", "Parting Shot", "Spectral Thief"],
 
   duskull: ["Bulk Up", "Drain Punch", "Recover", "Rage Fist"],
   dusclops: ["Bulk Up", "Drain Punch", "Recover", "Rage Fist"],
@@ -179,14 +179,14 @@ export const pokerogue_pokemonEggMoves_Hoenn = {
   tropius: ["Stuff Cheeks", "Earth Power", "Apple Acid", "Sappy Seed"],
 
   absol: ["Kowtow Cleave", "Sacred Sword", "Psyblade", "Bitter Blade"],
-  mega_absol: ["Kowtow Cleave", "Sacred Sword", "Psyblade", "Bitter Blade"],
+  absol_mega: ["Kowtow Cleave", "Sacred Sword", "Psyblade", "Bitter Blade"],
 
   wynaut: ["Recover", "Shed Tail", "Taunt", "Comeuppance"],
   wobbuffet: ["Recover", "Shed Tail", "Taunt", "Comeuppance"],
 
   snorunt: ["Sparkly Swirl", "Nasty Plot", "Earth Power", "Blood Moon"],
   glalie: ["Sparkly Swirl", "Nasty Plot", "Earth Power", "Blood Moon"],
-  mega_glalie: ["Sparkly Swirl", "Nasty Plot", "Earth Power", "Blood Moon"],
+  glalie_mega: ["Sparkly Swirl", "Nasty Plot", "Earth Power", "Blood Moon"],
   froslass: ["Sparkly Swirl", "Nasty Plot", "Earth Power", "Blood Moon"],
 
   spheal: ["Flip Turn", "Freeze Dry", "Slack Off", "Steam Eruption"],
@@ -204,12 +204,12 @@ export const pokerogue_pokemonEggMoves_Hoenn = {
   bagon: ["Headlong Rush", "Fire Lash", "Dragon Dance", "Dragon Darts"],
   shelgon: ["Headlong Rush", "Fire Lash", "Dragon Dance", "Dragon Darts"],
   salamence: ["Headlong Rush", "Fire Lash", "Dragon Dance", "Dragon Darts"],
-  mega_salamence: ["Headlong Rush", "Fire Lash", "Dragon Dance", "Dragon Darts"],
+  salamence_mega: ["Headlong Rush", "Fire Lash", "Dragon Dance", "Dragon Darts"],
 
   beldum: ["Headlong Rush", "Drain Punch", "Ice Spinner", "Shift Gear"],
   metang: ["Headlong Rush", "Drain Punch", "Ice Spinner", "Shift Gear"],
   metagross: ["Headlong Rush", "Drain Punch", "Ice Spinner", "Shift Gear"],
-  mega_metagross: ["Headlong Rush", "Drain Punch", "Ice Spinner", "Shift Gear"],
+  metagross_mega: ["Headlong Rush", "Drain Punch", "Ice Spinner", "Shift Gear"],
 
   regirock: ["Stone Axe", "Body Press", "Shore Up", "Salt Cure"],
 
@@ -218,19 +218,19 @@ export const pokerogue_pokemonEggMoves_Hoenn = {
   registeel: ["Body Press", "Sizzly Slide", "Recover", "Gigaton Hammer"],
 
   latias: ["Core Enforcer", "Fusion Flare", "Sparkly Swirl", "Mystical Power"],
-  mega_latias: ["Core Enforcer", "Fusion Flare", "Sparkly Swirl", "Mystical Power"],
+  latias_mega: ["Core Enforcer", "Fusion Flare", "Sparkly Swirl", "Mystical Power"],
 
   latios: ["Core Enforcer", "Blue Flare", "Nasty Plot", "Tachyon Cutter"],
-  mega_latios: ["Core Enforcer", "Blue Flare", "Nasty Plot", "Tachyon Cutter"],
+  latios_mega: ["Core Enforcer", "Blue Flare", "Nasty Plot", "Tachyon Cutter"],
 
   kyogre: ["Recover", "Hurricane", "Freezy Frost", "Wildbolt Storm"],
-  primal_kyogre: ["Recover", "Hurricane", "Freezy Frost", "Wildbolt Storm"],
+  kyogre_primal: ["Recover", "Hurricane", "Freezy Frost", "Wildbolt Storm"],
 
   groudon: ["Stone Axe", "Solar Blade", "Morning Sun", "Sacred Fire"],
-  primal_groudon: ["Stone Axe", "Solar Blade", "Morning Sun", "Sacred Fire"],
+  groudon_primal: ["Stone Axe", "Solar Blade", "Morning Sun", "Sacred Fire"],
 
   rayquaza: ["V Create", "Dragon Darts", "Core Enforcer", "Oblivion Wing"],
-  mega_rayquaza: ["V Create", "Dragon Darts", "Core Enforcer", "Oblivion Wing"],
+  rayquaza_mega: ["V Create", "Dragon Darts", "Core Enforcer", "Oblivion Wing"],
 
   jirachi: ["Tachyon Cutter", "Triple Arrows", "Rock Slide", "Shell Smash"],
 
